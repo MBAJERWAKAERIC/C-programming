a10dd0d00dff425e4410f3996931b07aeac81430
mbajerwakaeric <erickmbanjerwaka@gmail.com>
2023-06-16 12:31:55 +0300

    c programming repos setup

---
+++ Helloworld/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int age = 25;
+    printf("Age: %d", age);
+
+    age = 31;
+    printf("\nNew age: % , age");
+    return 0;
+}
---
+++ helloc/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main()
+{
+/* my first program in C */
+printf("Hello, World! \n");
+return 0;
+}
